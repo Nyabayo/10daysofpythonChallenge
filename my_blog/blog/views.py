060fdab880e1c284b126c987fc53cc09453b2cf0
Nyabayo <ernestosindo9@gmail.com>
2025-02-27 19:20:58 +0300

    Day4

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
+def home(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'blog/home.html', {'posts': posts})
